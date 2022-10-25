fb0b1bd4a1a28c7bca016616b62de6a6506b0ded
Tierry Pierre <pierre.tierry40@gmail.com>
2022-10-25 15:54:41 -0400

    finished

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let solution = this.currentTime / 6000;
+
+    return Math.floor(solution);
   }
 
   getSeconds() {
-    // ... your code goes here
+    let solution = (this.currentTime / 100) % 60;
+
+    return solution;
+  }
+
+  getCentiseconds() {
+    let solution = this.currentTime % 100;
+
+    return solution;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    }
+    return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let min = this.computeTwoDigitNumber(this.getMinutes());
+    let sec = this.computeTwoDigitNumber(this.getSeconds());
+    let cent = this.computeTwoDigitNumber(this.getCentiseconds());
+    console.log(`${min}:${sec}.${cent}`);
+    return `${min}:${sec}.${cent}`;
   }
 }
